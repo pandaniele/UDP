ac4ebda9f504534dfefcf4a2b9e5cb703656f3b5
Daniele Pandolfo <cocomelone46@gmail.com>
2022-05-12 16:33:48 +0200

    UDP

---
+++ src/udptime/MainUdpServer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udptime;
+
+/**
+ *
+ * @author pandl
+ */
+public class MainUdpServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      UDPServer us=new UDPServer();
+      us.ascolto();
+    }
+    
+}
